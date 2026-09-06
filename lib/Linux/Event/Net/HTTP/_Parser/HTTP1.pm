7770081df890e0319ae28c9287958d8568f38301
Joshua S. Day <haxmeister@hotmail.com>
2026-09-05 20:22:57 -0500

    Add private HTTP/1 parser loader

---
+++ lib/Linux/Event/Net/HTTP/_Parser/HTTP1.pm
@@ -0,0 +1,13 @@
+package Linux::Event::Net::HTTP::_Parser::HTTP1;
+use v5.36;
+use strict;
+use warnings;
+
+use Linux::Event::Net::HTTP ();
+use XSLoader ();
+
+our $VERSION = $Linux::Event::Net::HTTP::VERSION;
+
+XSLoader::load(__PACKAGE__, $VERSION);
+
+1;
