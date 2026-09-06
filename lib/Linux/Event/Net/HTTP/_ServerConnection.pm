02e2f195b3e55da4f05c2d9193711aea5b9fb363
Joshua S. Day <haxmeister@hotmail.com>
2026-09-06 11:41:48 -0500

    Add private HTTP server connection adapter

---
+++ lib/Linux/Event/Net/HTTP/_ServerConnection.pm
@@ -0,0 +1,32 @@
+package Linux::Event::Net::HTTP::_ServerConnection;
+use v5.36;
+use strict;
+use warnings;
+
+use parent 'Linux::Event::Net::HTTP::Connection';
+
+use Carp qw(croak);
+
+our $VERSION = '0.001';
+
+sub new ($class, %option) {
+    my $state = $option{data};
+    croak 'HTTP Server accepted connection state is missing'
+        if ref($state) ne 'HASH' || !$state->{_http_server_state};
+
+    my $connection_class = $state->{connection_class};
+    croak 'HTTP Server connection_class cannot use the private adapter directly'
+        if $connection_class eq __PACKAGE__;
+
+    $option{data} = $state->{data};
+
+    my $callbacks = $state->{callbacks};
+    for my $name (qw(on_request on_body on_request_end)) {
+        $option{$name} = $callbacks->{$name}
+            if exists $callbacks->{$name};
+    }
+
+    return $connection_class->new(%option);
+}
+
+1;
