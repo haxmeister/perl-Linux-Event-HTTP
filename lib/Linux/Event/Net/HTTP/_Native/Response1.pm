d3a4afe9815b56fc974694bcf6e45b30fa6cc37d
Joshua S. Day <haxmeister@hotmail.com>
2026-09-07 13:35:57 -0500

    Retire dedicated native final-response extension

--- lib/Linux/Event/Net/HTTP/_Native/Response1.pm
+++ lib/Linux/Event/Net/HTTP/_Native/Response1.pm
@@ -4,11 +4,15 @@ use strict;
 use warnings;
 
 use Linux::Event::Net::HTTP ();
-use Linux::Event::Net::HTTP::Request ();
-use XSLoader ();
 
 our $VERSION = $Linux::Event::Net::HTTP::VERSION;
 
-XSLoader::load(__PACKAGE__, $VERSION);
+# Transitional compatibility shim for the old private Connection engine.
+# Returning undef tells that engine to use the ordinary Response state machine.
+# The dedicated XS implementation was benchmark-specific and is intentionally
+# retired under the Linux::Event ecosystem protocol policy.
+sub build_default_final ($class, $request, $body) {
+    return undef;
+}
 
 1;
