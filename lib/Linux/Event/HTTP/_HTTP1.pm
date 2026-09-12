bae358745e0a1094e48b845e9f6e2d5069197bfe
Joshua S. Day <haxmeister@hotmail.com>
2026-09-12 18:47:45 -0500

    Bootstrap shared Request version accessor

--- lib/Linux/Event/HTTP/_HTTP1.pm
+++ lib/Linux/Event/HTTP/_HTTP1.pm
@@ -8,6 +8,14 @@ our $VERSION = '0.001';
 require XSLoader;
 XSLoader::load(__PACKAGE__);
 
+# Native parsing can create a Linux::Event::HTTP::Request without loading
+# Request.pm. Keep the protocol-neutral version() getter available in that
+# parser-only case; Request.pm replaces it with the full local/native wrapper
+# when the public message class is loaded.
+sub Linux::Event::HTTP::Request::version ($self) {
+    return $self->http_version;
+}
+
 sub CLONE_SKIP { 1 }
 
 1;
