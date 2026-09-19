60259053f38483af1350cdf3c2d508d3564d29ef
Joshua S. Day <haxmeister@hotmail.com>
2026-09-19 18:42:20 -0500

    bench: isolate fused callback boundary

--- bench/servers/linuxevent-http.pl
+++ bench/servers/linuxevent-http.pl
@@ -14,6 +14,49 @@ our $READ_BUDGET_BYTES = 0 + ($ENV{BENCH_READ_BUDGET_BYTES} // 0);
 my $payload = 'x' x $response_bytes;
 
 {
+    package Linux::Event::HTTP::Bench::LegacyCallbackCompareConnection;
+    use parent 'Linux::Event::HTTP::Server::Connection';
+
+    sub stream_tuning ($class) {
+        return read_budget_bytes => $main::READ_BUDGET_BYTES;
+    }
+
+    sub on_request ($self, $request, $response) {
+        $response->body($self->data->{payload});
+        return;
+    }
+
+    sub _invoke_http_callback ($self, $handler, $request, $response, @extra) {
+        return 1 if !$handler;
+
+        my $ok;
+        {
+            local $self->{_http_dispatching} = 1;
+            $ok = eval {
+                $handler->($self, $request, $response, @extra);
+                1;
+            };
+        }
+
+        if (!$ok) {
+            $self->_fail_active_transaction(500, $request, $response);
+            return 0;
+        }
+
+        my $ready = eval {
+            $self->_response_body_ready($response);
+            1;
+        };
+        if (!$ready) {
+            $self->_fail_active_transaction(500, $request, $response);
+            return 0;
+        }
+
+        return 1;
+    }
+}
+
+{
     package Linux::Event::HTTP::Bench::NaturalCompareConnection;
     use parent 'Linux::Event::HTTP::Server::Connection';
 
@@ -47,9 +90,11 @@ my $payload = 'x' x $response_bytes;
 
 my $connection_class = $mode eq 'natural'
     ? 'Linux::Event::HTTP::Bench::NaturalCompareConnection'
-    : $mode eq 'request-end'
-        ? 'Linux::Event::HTTP::Bench::RequestEndCompareConnection'
-        : die "unknown BENCH_LINUXEVENT_MODE: $mode\n";
+    : $mode eq 'legacy-callback'
+        ? 'Linux::Event::HTTP::Bench::LegacyCallbackCompareConnection'
+        : $mode eq 'request-end'
+            ? 'Linux::Event::HTTP::Bench::RequestEndCompareConnection'
+            : die "unknown BENCH_LINUXEVENT_MODE: $mode\n";
 
 my $loop = Linux::Event::Loop->new;
 my $server = Linux::Event::HTTP::Server->new(
