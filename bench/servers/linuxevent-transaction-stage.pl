83623f21a42fc384b72bb3ed7367c641eb0f39b9
Joshua S. Day <haxmeister@hotmail.com>
2026-09-19 22:36:25 -0500

    bench: isolate Response header body and framing costs

--- bench/servers/linuxevent-transaction-stage.pl
+++ bench/servers/linuxevent-transaction-stage.pl
@@ -15,7 +15,7 @@ my $response_bytes = 0 + ($ENV{BENCH_RESPONSE_BYTES} // 32);
 our $READ_BUDGET_BYTES = 0 + ($ENV{BENCH_READ_BUDGET_BYTES} // 0);
 our $STAGE = $ENV{BENCH_TRANSACTION_STAGE} // die "BENCH_TRANSACTION_STAGE is required\n";
 die "unknown BENCH_TRANSACTION_STAGE=$STAGE\n"
-    if $STAGE !~ /\A(?:parse|bound|fastbound|state|faststate|callbacks|fused|eligibility|build|mark|commit|complete|checked|bodyless|current_parse|current_response|current_api|current_frame|current_callback|current_head|current_send|current_checked|current_bodyless)\z/;
+    if $STAGE !~ /\A(?:parse|bound|fastbound|state|faststate|callbacks|fused|eligibility|build|mark|commit|complete|checked|bodyless|current_parse|current_response|current_header|current_api|current_frame|current_callback|current_head|current_send|current_checked|current_bodyless)\z/;
 
 my $payload = 'x' x $response_bytes;
 my $wire = "HTTP/1.1 200 OK\r\nContent-Length: $response_bytes\r\n\r\n$payload";
@@ -132,6 +132,14 @@ my $wire_ct = "HTTP/1.1 200 OK\r\n"
                 next;
             }
 
+            if ($main::STAGE eq 'current_header') {
+                $response->header(
+                    'Content-Type', 'application/octet-stream',
+                );
+                $self->write($self->data->{wire_ct});
+                next;
+            }
+
             if ($main::STAGE eq 'current_api'
                 || $main::STAGE eq 'current_frame'
                 || $main::STAGE eq 'current_head') {
@@ -145,9 +153,19 @@ my $wire_ct = "HTTP/1.1 200 OK\r\n"
                     next;
                 }
 
-                $response->header(
-                    'Content-Length', length($self->data->{payload}),
-                );
+                # Mirror the real scalar-final path: generated framing metadata
+                # is appended internally, not routed through public header().
+                my $headers = $response->{headers};
+                my $pair = [
+                    'Content-Length',
+                    '' . length($self->data->{payload}),
+                ];
+                if (@$headers) {
+                    push @$headers, $pair;
+                } else {
+                    $response->{headers} = [ $pair ];
+                }
+
                 if ($main::STAGE eq 'current_frame') {
                     $self->write($self->data->{wire_ct});
                 } else {
@@ -165,10 +183,23 @@ my $wire_ct = "HTTP/1.1 200 OK\r\n"
                     $handler, $request, $response,
                 );
                 last if !$ok;
+
+                my $headers = $response->{headers};
+                my $pair = [
+                    'Content-Length',
+                    '' . length($self->data->{payload}),
+                ];
+                if (@$headers) {
+                    push @$headers, $pair;
+                } else {
+                    $response->{headers} = [ $pair ];
+                }
+                my $head = $response->_serialize_head('1.1');
+
                 Linux::Event::HTTP::Server::Connection::_clear_transaction(
                     $self,
                 );
-                $self->write($self->data->{wire_ct});
+                $self->write($head . $self->data->{payload});
                 next;
             }
 
