6b0b6816f2160b8b8b204c24cb0a9b3d3485f839
Joshua S. Day <haxmeister@hotmail.com>
2026-09-20 01:57:10 -0500

    bench: expose minimal Response allocation stage

--- bench/run-http-transaction-ladder.pl
+++ bench/run-http-transaction-ladder.pl
@@ -24,6 +24,12 @@ my %case = (
         stage => 'current_parse',
         description => 'Current Perl input buffer plus pico/native Request construction; prebuilt Content-Type response write',
     },
+    current_response_empty => {
+        label => 'C2a + empty server Response object',
+        command => [$^X, '-Mblib', "$Bin/servers/linuxevent-transaction-stage.pl"],
+        stage => 'current_response_empty',
+        description => 'Parsed Request plus an otherwise-empty blessed Response hash; estimates the lower bound for implicit server defaults',
+    },
     current_response => {
         label => 'C2 + trusted sparse Response',
         command => [$^X, '-Mblib', "$Bin/servers/linuxevent-transaction-stage.pl"],
@@ -242,7 +248,7 @@ die "timeout must be > 0\n" if $timeout <= 0;
 die "read-budget-bytes must be >= 0\n" if $read_budget_bytes < 0;
 
 my $request_wire = "GET /bench HTTP/1.1\r\nHost: benchmark.test\r\n\r\n";
-my @names = qw(current_parse current_response current_header current_api current_frame current_head current_exchange_minimal current_exchange_nomark current_exchange current_callback current_send current_checked current_bodyless current_http);
+my @names = qw(current_parse current_response_empty current_response current_header current_api current_frame current_head current_exchange_minimal current_exchange_nomark current_exchange current_callback current_send current_checked current_bodyless current_http);
 if (defined $case_list) {
     my %known = map { $_ => 1 } @names;
     my @selected = grep { length } split /,/, $case_list;
