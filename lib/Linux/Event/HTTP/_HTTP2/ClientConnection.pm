6acd9e93a538400cfdb97d1e58bcd2efef49523f
Joshua S. Day <haxmeister@hotmail.com>
2026-09-25 21:58:58 -0500

    http2: contain malformed protocol input per connection

--- lib/Linux/Event/HTTP/_HTTP2/ClientConnection.pm
+++ lib/Linux/Event/HTTP/_HTTP2/ClientConnection.pm
@@ -10,7 +10,21 @@ our $VERSION = '0.002';
 sub on_data ($self, $bytes) {
     my $executor = $self->{_http2_executor}
         or die 'HTTP/2 client connection has no executor';
-    $executor->input($bytes);
+
+    my $ok = eval {
+        $executor->input($bytes);
+        1;
+    };
+    if (!$ok) {
+        my $error = "$@";
+        $error =~ s/\s+\z//;
+        $executor->close(
+            $error ne '' ? "HTTP/2 protocol input failed: $error"
+                         : 'HTTP/2 protocol input failed',
+        ) if $executor->can('close');
+        delete $self->{_http2_executor};
+        $self->close if !$self->is_closed;
+    }
     return;
 }
 
